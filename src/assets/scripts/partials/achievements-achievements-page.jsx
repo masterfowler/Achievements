e68eb9cd1a1c0a5d8e27e82c23149489ea2d548d
MasterFowler <masterjfowler@gmail.com>
2016-03-29 20:27:21 +1300

    Started achievements page. Updated colour scheme.

--- src/assets/scripts/partials/achievements-achievements-page.jsx
+++ src/assets/scripts/partials/achievements-achievements-page.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
 require('bootstrap');
+require('style/achievements-achievements-page.scss');
 
 class AchievementsPage extends React.Component{
     
     render() {
         return (
             <div className="achievements-panel">
-                <h1>Achievements</h1>       
+                <div className="row">
+                    <div className="col-md-2">
+                        <div className="thumbnail unearned-achievement">
+                            
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="thumbnail unearned-achievement">
+                            
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="thumbnail unearned-achievement">
+                            
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="thumbnail unearned-achievement">
+                            
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="thumbnail unearned-achievement">
+                            
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="thumbnail unearned-achievement">
+                            
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="thumbnail unearned-achievement">
+                            
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="thumbnail unearned-achievement">
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
