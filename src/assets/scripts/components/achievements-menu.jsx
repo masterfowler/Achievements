53cc1819068a28ca8690811aa5c851425da50c6e
MasterFowler <masterjfowler@gmail.com>
2016-04-12 21:36:00 +1200

    Added facebook login

--- src/assets/scripts/components/achievements-menu.jsx
+++ src/assets/scripts/components/achievements-menu.jsx
@@ -37,6 +37,9 @@ class AchievementsMenu extends React.Component{
         
         this.navItemClicked = this.navItemClicked.bind(this);
         this.urlChangedHandler = this.urlChangedHandler.bind(this);
+        this.statusChangeCallback = this.statusChangeCallback.bind(this);
+        this.checkLoginState = this.checkLoginState.bind(this);
+        
         this.state = this._getInitialState();
     }
     
@@ -49,12 +52,62 @@ class AchievementsMenu extends React.Component{
     
     componentDidMount() {
 		window.addEventListener('hashchange', this.urlChangedHandler);
-	}
-
+        this.initFacebook();
+    }
 	
 	componentWillUnmount() {
 		window.removeEventListener('hashchange', this.urlChangedHandler);
 	}
+    
+    initFacebook() {
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '',
+                cookie     : true,  // enable cookies to allow the server to access
+                                    // the session
+                xfbml      : true,  // parse social plugins on this page
+                version    : 'v2.5' // use version 2.5
+            });
+
+            // Now that we've initialized the JavaScript SDK, we call
+            // FB.getLoginStatus().  This function gets the state of the
+            // person visiting this page and can return one of three states to
+            // the callback you provide.  They can be:
+            //
+            // 1. Logged into your app ('connected')
+            // 2. Logged into Facebook, but not your app ('not_authorized')
+            // 3. Not logged into Facebook and can't tell if they are logged into
+            //    your app or not.
+            //
+            // These three cases are handled in the callback function.
+            FB.getLoginStatus(function(response) {
+                this.statusChangeCallback(response);
+            }.bind(this));
+        }.bind(this);
+        // Load the SDK asynchronously
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    }
+    
+    // This is called with the results from from FB.getLoginStatus().
+    statusChangeCallback(response) {
+        console.log('statusChangeCallback');
+        console.log(response);
+    }
+    
+    // This function is called when someone finishes with the Login
+    // Button.  See the onlogin handler attached to it in the sample
+    // code below.
+    checkLoginState() {
+        FB.getLoginStatus(function(response) {
+            this.statusChangeCallback(response);
+        }.bind(this));
+    }
     
     urlChangedHandler() {
         var route = window.location.hash;
@@ -117,7 +170,7 @@ class AchievementsMenu extends React.Component{
                         <ul className="nav navbar-nav achievements-links">
                             {navItems}
                         </ul>
-                        <button type="button" className="btn btn-default navbar-btn navbar-right">Sign in</button>
+                        <div className="fb-login-button pull-right" onlogin="checkLoginState" data-size="medium" data-show-faces="true" data-auto-logout-link="true"></div>
                     </div>
                 </div>
             </nav>      
